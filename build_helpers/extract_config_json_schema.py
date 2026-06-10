ea7a2e526469b814c5fedb5658d12dc5d7c4b67b
sunlightcold <840289075@qq.com>
2026-06-10 22:14:16 +0800

    ci: stabilize schema generation newlines

--- build_helpers/extract_config_json_schema.py
+++ build_helpers/extract_config_json_schema.py
@@ -22,7 +22,7 @@ def extract_config_json_schema():
         from freqtrade.config_schema import CONF_SCHEMA
 
     schema_filename = Path(__file__).parent / "schema.json"
-    with schema_filename.open("w") as f:
+    with schema_filename.open("w", newline="\n") as f:
         rapidjson.dump(CONF_SCHEMA, f, indent=2)
 
 
